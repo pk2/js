2f93818bfc963f7e1f131581394c0bb92c18a5e5
piotr <piotr.karczemski+github@gmail.com>
2021-09-22 22:43:20 +0200

    arrow function

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+/*
 //function declaration
+const age1 = calcAge1(1991);
 function calcAge1(birthYear) {
     return 2037 - birthYear;
-}
 
-const age1 = calcAge1(1991);
+}
 console.log(age1);
 //function expression
 const calcAge2 = function (birthYear) {
@@ -11,3 +12,12 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1991);
 console.log(age2);
+*/
+
+//function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+const calcAge3 = birthYear => 2037 - birthYear
+const age3 = calcAge3(1991);
+console.log(age3);
