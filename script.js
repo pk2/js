c91e96a827dd1fb567931c93ef3aff30bc32a6b8
piotr <piotr.karczemski+github@gmail.com>
2021-09-23 19:49:14 +0200

    function in function

--- script.js
+++ script.js
@@ -21,11 +21,23 @@ console.log(age2);
 // const calcAge3 = birthYear => 2037 - birthYear
 // const age3 = calcAge3(1991);
 // console.log(age3);
+//
+// const yearsUntilRetirement = (birthYear,firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return `${firstName} retires in ${retirement} years`;
+// }
+// console.log(yearsUntilRetirement(1991,'Piotr'));
+// console.log(yearsUntilRetirement(1980,'Bob'));
 
-const yearsUntilRetirement = (birthYear,firstName) => {
-    const age = 2037 - birthYear;
-    const retirement = 65 - age;
-    return `${firstName} retires in ${retirement} years`;
+function cutFruitPieces(fruit) {
+    return fruit * 4;
 }
-console.log(yearsUntilRetirement(1991,'Piotr'));
-console.log(yearsUntilRetirement(1980,'Bob'));
+
+function fruitProcessor(apples, oranges) {
+    const applePices = cutFruitPieces(apples);
+    const orangePices = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePices} apples and ${orangePices} oranges.`;
+    return juice;
+}
+console.log(fruitProcessor(2,3));
